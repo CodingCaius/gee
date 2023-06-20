d615f4f5c776aa52be68761d07565ff5e7f62669
caius <3085481534@qq.com>
2023-06-20 15:27:33 +0800

    update context

--- gee-web/day2-context/main.go
+++ gee-web/day2-context/main.go
@@ -1,26 +1,6 @@
 package main
 
-/*
-(1)
-$ curl -i http://localhost:9999/
-HTTP/1.1 200 OK
-Date: Mon, 12 Aug 2019 16:52:52 GMT
-Content-Length: 18
-Content-Type: text/html; charset=utf-8
-<h1>Hello Gee</h1>
 
-(2)
-$ curl "http://localhost:9999/hello?name=geektutu"
-hello geektutu, you're at /hello
-
-(3)
-$ curl "http://localhost:9999/login" -X POST -d 'username=geektutu&password=1234'
-{"password":"1234","username":"geektutu"}
-
-(4)
-$ curl "http://localhost:9999/xxx"
-404 NOT FOUND: /xxx
-*/
 
 import (
 	"net/http"
@@ -34,7 +14,6 @@ func main() {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 	r.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=geektutu
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
