083c31df49efe604ca2e19fc913d7470edeb3769
caius <3085481534@qq.com>
2023-09-19 21:42:39 +0800

    利用反射完成结构体和数据库表结构的映射

---
+++ gee-orm/day2-对象表结构映射/schema/schema.go
@@ -0,0 +1,60 @@
+// Schema对象通常用于ORM（对象-关系映射）框架中，它描述了数据表的结构，包括表名、字段名、字段类型等信息
+
+package schema
+
+import (
+	"geeorm/dialect"
+	"go/ast"
+	"reflect"
+)
+
+// Field represents a column of database
+type Field struct {
+	Name string
+	Type string
+	Tag  string
+}
+
+// Schema represents a table of database
+type Schema struct {
+	Model      interface{} // 关联的 Go 结构体（模型）
+	Name       string
+	Fields     []*Field
+	FieldNames []string
+	fieldMap   map[string]*Field // 一个映射，用于将字段名称映射到对应的 Field 结构体
+}
+
+// GetField 根据字段名称获取对应的 Field 结构体
+func (schema *Schema) GetField(name string) *Field {
+	return schema.fieldMap[name]
+}
+
+// Parse 解析传入的目标模型，提取其中可导出的字段信息，创建Field对象，并将这些信息填充到Schema对象中，以便后续在ORM操作中使用
+func Parse(dest interface{}, d dialect.Dialect) *Schema {
+	// 使用反射（reflection）获取目标模型的类型
+	// 因为设计的入参是一个对象的指针，因此需要 reflect.Indirect() 获取指针指向的实例
+	modelType := reflect.Indirect(reflect.ValueOf(dest)).Type()
+	schema := &Schema{
+		Model:    dest,
+		Name:     modelType.Name(),
+		fieldMap: make(map[string]*Field),
+	}
+
+	for i := 0; i < modelType.NumField(); i++ {
+		p := modelType.Field(i)
+		// 检查字段是否是可导出的（即字段名是否以大写字母开头），同时排除匿名字段（非导出的字段）
+		if !p.Anonymous && ast.IsExported(p.Name) {
+			field := &Field{
+				Name: p.Name,
+				Type: d.DataTypeOf(reflect.Indirect(reflect.New(p.Type))),
+			}
+			if v, ok := p.Tag.Lookup("geeorm"); ok {
+				field.Tag = v
+			}
+			schema.Fields = append(schema.Fields, field)
+			schema.FieldNames = append(schema.FieldNames, p.Name)
+			schema.fieldMap[p.Name] = field
+		}
+	}
+	return schema
+}
---
+++ gee-orm/day2-对象表结构映射/session/raw.go
@@ -0,0 +1,76 @@
+// 该包负责与数据库的交互
+// 将数据库表的增/删操作实现在子包 session 中
+
+package session
+
+import (
+	"database/sql"
+	"geeorm/dialect"
+	"geeorm/log"
+	"geeorm/schema"
+	"strings"
+)
+
+type Session struct {
+	db       *sql.DB // 使用 sql.Open() 方法连接数据库成功之后返回的指针
+	dialect  dialect.Dialect
+	refTable *schema.Schema  // 指向 *schema.Schema 类型的指针，表示与数据库表格相关的元数据信息
+	sql      strings.Builder // 用于构建SQL查询语句
+	sqlVars  []interface{}   // 用于存储SQL查询语句中的参数
+}
+
+// 用于创建一个新的 Session 对象
+func New(db *sql.DB, dialect dialect.Dialect) *Session {
+	return &Session{
+		db:      db,
+		dialect: dialect,
+	}
+}
+
+// 清除 Session 对象中的 SQL 查询语句和参数
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+}
+
+// 返回 Session 对象中的数据库连接对象 db
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+// 用于构建原始的 SQL 查询语句
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
+
+// 封装三个原生方法
+
+// Exec 用于执行原始的 SQL 查询或操作
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+// QueryRow 仅返回查询结果的第一行
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
+
+// QueryRows gets a list of records from db
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
