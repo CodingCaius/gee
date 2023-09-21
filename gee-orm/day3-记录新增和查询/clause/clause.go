ac64921b9a70175762f8f225f4f8da77c8cae375
caius <3085481534@qq.com>
2023-09-21 22:46:27 +0800

    添加和查询功能

---
+++ gee-orm/day3-记录新增和查询/clause/clause.go
@@ -0,0 +1,49 @@
+// 拼接各个独立的子句
+
+// 这个包的主要目的是将不同类型的 SQL 子句和相关的变量进行管理和拼接，使构建复杂的 SQL 查询语句变得更加方便。
+// 在使用时，可以调用 Set 方法来设置不同类型的 SQL 子句，然后调用 Build 方法来构建完整的查询语句
+
+package clause
+
+import "strings"
+
+type Clause struct {
+	sql     map[Type]string        // 用于存储不同类型的 SQL 子句
+	sqlVars map[Type][]interface{} // 用于存储与每个子句相关的变量
+}
+
+type Type int
+
+const (
+	INSERT Type = iota
+	VALUES
+	SELECT
+	LIMIT
+	WHERE
+	ORDERBY
+)
+
+// Set 根据 Type 调用对应的 generator，生成该子句对应的 SQL 语句
+func (c *Clause) Set(name Type, vars ...interface{}) {
+	if c.sql == nil {
+		c.sql = make(map[Type]string)
+		c.sqlVars = make(map[Type][]interface{})
+	}
+	sql, vars := generators[name](vars...)
+	c.sql[name] = sql
+	c.sqlVars[name] = vars
+}
+
+// Build 构建最终的 SQL 查询语句
+func (c *Clause) Build(orders ...Type) (string, []interface{}) {
+	// 可变参数 orders，存储的是类型，表示需要构建的 SQL 子句的顺序
+	var sqls []string
+	var vars []interface{}
+	for _, order := range orders {
+		if sql, ok := c.sql[order]; ok {
+			sqls = append(sqls, sql)
+			vars = append(vars, c.sqlVars[order]...)
+		}
+	}
+	return strings.Join(sqls, " "), vars
+}
---
+++ gee-orm/day3-记录新增和查询/clause/generator.go
@@ -0,0 +1,89 @@
+// 实现各个子句的生成规则
+
+package clause
+
+import (
+	"fmt"
+	"strings"
+)
+
+// 自定义的函数类型，用于表示生成 SQL 语句的函数
+type generator func(values ...interface{}) (string, []interface{})
+
+var generators map[Type]generator
+
+func init() {
+	generators = make(map[Type]generator)
+	generators[INSERT] = _insert
+	generators[VALUES] = _values
+	generators[SELECT] = _select
+	generators[LIMIT] = _limit
+	generators[WHERE] = _where
+	generators[ORDERBY] = _orderBy
+}
+
+// 将这些子句组合在一起以构建完整的 SQL 查询语句
+
+// genBindVars 生成一组绑定变量(?)的字符串，用于 SQL 查询中的参数绑定
+func genBindVars(num int) string {
+	var vars []string
+	for i := 0; i < num; i++ {
+		vars = append(vars, "?")
+	}
+	return strings.Join(vars, ", ")
+}
+
+// _insert 生成 INSERT 语句
+func _insert(values ...interface{}) (string, []interface{}) {
+	// INSERT INTO $tableName ($fields)
+	tableName := values[0]
+	// 类型断言，然后用逗号拼接
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("INSERT INTO %s (%v)", tableName, fields), []interface{}{}
+}
+
+// _values 生成 VALUES 子句
+func _values(values ...interface{}) (string, []interface{}) {
+	// VALUES ($v1), ($v2), ...
+	var bindStr string
+	var sql strings.Builder
+	var vars []interface{}
+	sql.WriteString("VALUES ")
+	for i, value := range values {
+		v := value.([]interface{})
+		if bindStr == "" {
+			bindStr = genBindVars(len(v))
+		}
+		sql.WriteString(fmt.Sprintf("(%v)", bindStr))
+		if i+1 != len(values) {
+			sql.WriteString(", ")
+		}
+		vars = append(vars, v...)
+	}
+	return sql.String(), vars
+
+}
+
+// _select 生成 SELECT 语句
+func _select(values ...interface{}) (string, []interface{}) {
+	// SELECT $fields FROM $tableName
+	tableName := values[0]
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("SELECT %v FROM %s", fields, tableName), []interface{}{}
+}
+
+// _limit 生成 LIMIT 子句
+func _limit(values ...interface{}) (string, []interface{}) {
+	// LIMIT $num
+	return "LIMIT ?", values
+}
+
+func _where(values ...interface{}) (string, []interface{}) {
+	// WHERE $desc
+	desc, vars := values[0], values[1:]
+	return fmt.Sprintf("WHERE %s", desc), vars
+}
+
+func _orderBy(values ...interface{}) (string, []interface{}) {
+	return fmt.Sprintf("ORDER BY %s", values[0]), []interface{}{}
+}
